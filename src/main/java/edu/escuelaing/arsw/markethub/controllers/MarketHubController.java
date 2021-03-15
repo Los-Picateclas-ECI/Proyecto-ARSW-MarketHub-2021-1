53b054860d03fa3d1d169c47c9543968fdb3cf88
Andres Mateo Calderon Ortega <andres.calderon-o@mail.escuelaing.edu.co>
2021-03-15 17:47:59 -0500

    refactor front

--- src/main/java/edu/escuelaing/arsw/markethub/controllers/MarketHubController.java
+++ src/main/java/edu/escuelaing/arsw/markethub/controllers/MarketHubController.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
  * Controlador principal del proyecto MarketHub
  */
 @RestController
-// @RequestMapping(value = "/markethub")
 public class MarketHubController {
 
     /**
      * Clase de hello markethub
+     *
      * @return - String de bienvenido
      */
     @RequestMapping("/hello")
